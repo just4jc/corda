edd427d4162b36466e07d3033c6ba4d368498919
Jose Coll <jose.coll@r3cev.com>
2016-11-21 16:59:03 +0000

    Register custom RPC Kryo classes.

--- samples/bank-of-corda-demo/src/main/kotlin/net/corda/bank/plugin/BankOfCordaPlugin.kt
+++ samples/bank-of-corda-demo/src/main/kotlin/net/corda/bank/plugin/BankOfCordaPlugin.kt
@@ -1,7 +1,9 @@
 package net.corda.bank.plugin
 
+import com.esotericsoftware.kryo.Kryo
 import net.corda.bank.api.BankOfCordaWebApi
 import net.corda.bank.protocol.IssuerProtocol
+import net.corda.bank.protocol.IssuerProtocolResult
 import net.corda.core.contracts.Amount
 import net.corda.core.crypto.Party
 import net.corda.core.node.CordaPluginRegistry
@@ -11,7 +13,13 @@ class BankOfCordaPlugin : CordaPluginRegistry() {
     override val webApis: List<Class<*>> = listOf(BankOfCordaWebApi::class.java)
     // A list of protocols that are required for this cordapp
     override val requiredProtocols: Map<String, Set<String>> = mapOf(
-            IssuerProtocol.IssuanceRequester::class.java.name to setOf(Amount::class.java.name, Party::class.java.name)
+            IssuerProtocol.IssuanceRequester::class.java.name to setOf(Amount::class.java.name, String::class.java.name, String::class.java.name)
     )
     override val servicePlugins: List<Class<*>> = listOf(IssuerProtocol.Issuer.Service::class.java)
+
+    override fun registerRPCKryoTypes(kryo: Kryo): Boolean {
+                kryo.register(IssuerProtocolResult.Success::class.java)
+                kryo.register(IssuerProtocolResult.Failed::class.java)
+                return true
+    }
 }
