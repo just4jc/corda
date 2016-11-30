079160350cbe23b39c8f809a8a68e43f225af577
Jose Coll <jose.coll@r3cev.com>
2016-11-30 17:37:42 +0000

    Added Vault updates verification in RPC Integration test.

--- samples/bank-of-corda-demo/src/integration-test/kotlin/net/corda/bank/BankOfCordaRPCClientTest.kt
+++ samples/bank-of-corda-demo/src/integration-test/kotlin/net/corda/bank/BankOfCordaRPCClientTest.kt
@@ -6,6 +6,7 @@ import net.corda.bank.flow.IssuerFlowResult
 import net.corda.client.CordaRPCClient
 import net.corda.core.contracts.DOLLARS
 import net.corda.core.node.services.ServiceInfo
+import net.corda.core.node.services.Vault
 import net.corda.node.driver.driver
 import net.corda.node.services.User
 import net.corda.node.services.config.configureTestSSL
@@ -14,24 +15,77 @@ import net.corda.node.services.startFlowPermission
 import net.corda.node.services.transactions.SimpleNotaryService
 import net.corda.testing.getHostAndPort
 import org.junit.Test
+import rx.subjects.PublishSubject
 import kotlin.test.assertTrue
 
 class BankOfCordaRPCClientTest {
+
     @Test fun `test issuer flow via RPC`() {
         driver(dsl = {
             val user = User("user1", "test", permissions = setOf(startFlowPermission<IssuanceRequester>()))
             val nodeBankOfCorda = startNode("BankOfCorda", setOf(ServiceInfo(SimpleNotaryService.type)), arrayListOf(user)).get()
             val nodeBankOfCordaApiAddr = nodeBankOfCorda.config.getHostAndPort("artemisAddress")
             val bankOfCordaName = nodeBankOfCorda.nodeInfo.legalIdentity.name
-            val nodeBigCorporation = startNode("BigCorporation").get()
+            val nodeBigCorporation = startNode("BigCorporation", rpcUsers = arrayListOf(user)).get()
             val bigCorporationName = nodeBigCorporation.nodeInfo.legalIdentity.name
 
-            val client = CordaRPCClient(nodeBankOfCordaApiAddr, configureTestSSL())
-            client.start("user1","test")
-            val proxy = client.proxy()
+            // Bank of Corda RPC Client
+            val bocClient = CordaRPCClient(nodeBankOfCordaApiAddr, configureTestSSL())
+            bocClient.start("user1","test")
+            val bocProxy = bocClient.proxy()
 
-            val result = proxy.startFlow(::IssuanceRequester, 1000.DOLLARS, bigCorporationName, BOC_ISSUER_PARTY_REF, bankOfCordaName).returnValue.toBlocking().first()
+            // Big Corporation RPC Client
+            val bigCorpClient = CordaRPCClient(nodeBankOfCordaApiAddr, configureTestSSL())
+            bigCorpClient.start("user1","test")
+            val bigCorpProxy = bigCorpClient.proxy()
+
+            // Register for Bank of Corda Vault updates
+            val vaultUpdatesSubject = PublishSubject.create<Vault.Update>()
+            val (vault, vaultUpdates) = bocProxy.vaultAndUpdates()
+
+            // Register for Big Corporation Vault updates
+            val bigCorpVaultUpdatesSubject = PublishSubject.create<Vault.Update>()
+            val (vaultBigCorp, vaultUpdatesBigCorp) = bigCorpProxy.vaultAndUpdates()
+
+            // Kick-off actual Issuer Flow
+            val result = bocProxy.startFlow(::IssuanceRequester, 1000.DOLLARS, bigCorporationName, BOC_ISSUER_PARTY_REF, bankOfCordaName).returnValue.toBlocking().first()
             assertTrue { result is IssuerFlowResult.Success }
+
+            // Check Bank of Corda Vault Updates
+            val initialVaultUpdate = Vault.Update(setOf(), vault.toSet())
+            vaultUpdatesSubject.subscribe {
+                update ->
+                println("BOC ===> ${update} <===")
+                // EXPECT TO SEE an ISSUE and a MOVE
+            }
+            vaultUpdates.startWith(initialVaultUpdate).subscribe(vaultUpdatesSubject)
+
+            // Check Big Corporation Vault Updates
+            val bigCorpInitialVaultUpdate = Vault.Update(setOf(), vaultBigCorp.toSet())
+            bigCorpVaultUpdatesSubject.subscribe {
+                update ->
+                println("BigCorp ===> ${update} <===")
+                // EXPECT TO SEE an ISSUE and a MOVE
+            }
+            vaultUpdatesBigCorp.startWith(bigCorpInitialVaultUpdate).subscribe(bigCorpVaultUpdatesSubject)
+
+            // Check Big Corporation Vault Updates
+//            val bigCorpInitialVaultUpdate = Vault.Update(setOf(), vaultBigCorp.toSet())
+//            bigCorpVaultUpdatesSubject.expectEvents {
+//                sequence(
+//                        // ISSUE
+//                        expect { update ->
+//                            require(update.consumed.size == 0) { update.consumed.size }
+//                            require(update.produced.size == 1) { update.produced.size }
+//                        },
+//                        // MOVE
+//                        expect { update ->
+//                            require(update.consumed.size == 1) { update.consumed.size }
+//                            require(update.produced.size == 0) { update.produced.size }
+//                        }
+//                )
+//            }
+
         }, isDebug = true)
     }
 }
